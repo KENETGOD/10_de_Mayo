69f57b8f811c6610fa710c380df28fc934a8aeef
KENETGOD <cazareslachinoluisfernando@gmail.com>
2024-05-08 10:35:59 -0600

    Arreglo temporal

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
+function showCard() {
+  var nombre = document.getElementById('nombre').value;
+  var mensaje = document.getElementById('mensaje').value;
 
-// prevent the default behavior of the form
-document.getElementById("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  function showCard(nombre, mensaje) {
-    var card = document.getElementById("card");
-    var resMsg = document.getElementById("res-msg");
-    var resName = document.getElementById("res-name");
+  document.getElementById('res-name').textContent = nombre;
+  document.getElementById('res-msg').textContent = mensaje;
 
-    console.log(nombre, mensaje);
-
-    resName.innerHTML = nombre;
-    resMsg.innerHTML = mensaje;
-
-    card.style.display = "block";
-  }
-});
+  document.getElementById('formContainer').style.display = 'none';
+  document.getElementById('cardContainer').style.display = 'block';
+}
