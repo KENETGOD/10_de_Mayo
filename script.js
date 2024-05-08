e582c305ec85cc536aa46a82ae99f2e9c7e3a3bd
Obed GonHer <l21121524@morelia.tecnm.mx>
2024-05-08 10:17:18 -0600

    sin action en el form

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function showCard() {
-  var card = document.getElementById("card");
-  card.style.display = "block";
-}
+
+// prevent the default behavior of the form
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  function showCard(nombre, mensaje) {
+    var card = document.getElementById("card");
+    var resMsg = document.getElementById("res-msg");
+    var resName = document.getElementById("res-name");
+
+    console.log(nombre, mensaje);
+
+    resName.innerHTML = nombre;
+    resMsg.innerHTML = mensaje;
+
+    card.style.display = "block";
+  }
+});
